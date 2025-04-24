464ca0db81aefdfe3aa0d14c2bde924a101cc67e
boorapi <169002265+boorapi@users.noreply.github.com>
2025-04-25 00:18:34 +1200

    Add files via upload
    24/04/25 Finish work!

--- Account.java
+++ Account.java
@@ -13,56 +13,58 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.io.FileWriter; //to write file
 public class Account {
+    private static ArrayList<String> accountArray = new ArrayList<String>();//this array lit will store all the user information.
+    //This method will load all the data from csv file to "accountArray" ArrayList.
+    //It would only run once when the program start.
+    public static void loadAccount(){
+        File accountFile = new File("AccountData.csv");//create new file object called accountFile. 
+        try{
+            Scanner readFile = new Scanner(accountFile);//use Scanner to read the file
+            //while the file has next line
+            while(readFile.hasNextLine()){
+                accountArray.add(readFile.nextLine());//add that line to accountArray
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
     private String name;
     private String address;
     private String accountNum;
     private String accountType;
     private double balance;
-
-    int accountIndex = -1;
-    //so I can assign account number correctly
-    private static ArrayList<String> accountArray = new ArrayList<String>();
-    private static boolean loaded = false;
-    //this constructure methode will take in a name and then find the account info under that given name.
-    public static void loadAccount(){
-        File accountFile = new File("AccountData.csv");
-        if(!loaded){
-            try{
-                Scanner readFile = new Scanner(accountFile);
-                while(readFile.hasNextLine()){
-                    accountArray.add(readFile.nextLine());
-                }
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        }
-        loaded = true;
-    }
-    
+    private int accountIndex = -1;//This value will keep track of which index that particular account element is in.
+    //this constructure methode will take in a name and account number and then get the account information under the name.
     public Account (String name, String num){
         boolean keepCounting = true;
+        //loop through every element in accountArray
         for (String account : accountArray){
-            String[] value = account.split(",");
+            String[] value = account.split(",");//separate the account info by comma and store it in array called value.
+            
+            //keep counting the index of accountArray until keepCounting is false.
             if(keepCounting){
-                accountIndex++;
+                accountIndex++;//increment by 1 every times
             }
                 
-            //if the name is the same and the account number are the same.
+            //if the name (in lowercase so if the user type wrong case its still work)is the same and the account number are the same.
             if(value[0].toLowerCase().equals(name) && value[2].equals(num)){
-                keepCounting = false;
-                this.name = value[0];
-                this.address = value[1];
-                this.accountNum = value[2];
-                this.accountType = value[3];
-                this.balance = Double.valueOf(value[4]); 
+                keepCounting = false;//stop counting the index
+                this.name = value[0];//first value is name
+                this.address = value[1];//second value is the address
+                this.accountNum = value[2];//third one is the account number
+                this.accountType = value[3];//fourth one is the account type
+                this.balance = Double.valueOf(value[4]);//last one is balance which are convert from string to double 
             }
         }
     }   
     
-    //This method takes in name, address, account type in int e.g.1 for Everyday 2 for Savings and 3 for Current, and boolean to check if there already existing account.
-    //It will genarate account number and save the new data to the arraylist.
+    //This method will create account and save the new data to the arraylist.
+    //The method takes in name, address, account type in int e.g.1 for Everyday 2 for Savings and 3 for Current.
+    //It also genarate accoun number for you. If there is already an account under user name it increment the lase suffix by 1 
     public static String createAccount(String name, String address, int type){
-        Random random = new Random();
+        Random random = new Random();//create random odject to create random number.
+        // Determine the account type based on the integer input
         String accountType;
         switch (type){
             case 1 : accountType = "Everyday";
@@ -71,90 +73,100 @@ public class Account {
                 break;
             case 3 : accountType = "Current";
                 break;
-            default: accountType = "Unknown";
+            default: accountType = "Unknown";//// Fallback in case of invalid type
         }
+        //A fixed prefix of account number
         String accountNumber = "08-0101-";
+        //addpend 7 random digit to account number.
         for (int i=0; i<7; i++){
             int j = random.nextInt(9);
             accountNumber = accountNumber+j;
         }
         
-        int x = 0;
-        //A loop to check account that have highest last digit and that is under the same name.
+        int x = 0;//Used to track the highest "last digit" suffix for accounts with the same name
+        // Loop through all existing accounts to find the highest suffix for accounts with the same name
         for (String account : accountArray){
             String[] split = account.split(",");
-            //If the account is under this name
+            //If the account name match (case does not matter)
             if(split[0].toLowerCase().equals(name.toLowerCase())){
-                String num = split[2];
-                String[] value = num.split("-");
-                String i = value[3];
-                int lastNum = Integer.parseInt(i);
+                String num = split[2];//get the full account number
+                String[] value = num.split("-");// Split the account number by dashes
+                String i = value[3];// Get the suffix after the final dash
+                int lastNum = Integer.parseInt(i);// Convert suffix to integer
+                // Update x if this suffix is higher than the current max
                 if(lastNum>x){
                     x = lastNum;
                 }
             }
         }
+        //If there is account found, increment x to get to next suffix
         if(x>0){
             x++;
         }
+        // Append the new suffix to the account number with proper formatting (e.g. "-01")
         if(x>9){
             accountNumber = accountNumber+"-"+x;
         } else{
             accountNumber =accountNumber+"-0"+x;
         }
+        // Combine all account details into a single csv formatt
         String data = (name+","+address+","+accountNumber+","+accountType+","+"0.00"); 
-        accountArray.add(data);
-        return accountNumber;
+        accountArray.add(data);//add it to the accountArray
+        return accountNumber;//return account number so I can printed out to the user
     }
     
-    //If I call this method it will rewrite all the updated data to the account .csv file
+    //If I call this method it will write all the updated data from accountArray to the account csv file
     public static void writeToFile(){
-        File accountFile = new File("AccountData.csv");
+        File accountFile = new File("AccountData.csv");//create "AccountData" object
         // String variable called newLine, which will allow me to create new without using \n
         String newLine = System.getProperty("line.separator");
         try{
-            FileWriter writer = new FileWriter(accountFile);
+            FileWriter writer = new FileWriter(accountFile); // Create a FileWriter object to write data to the file
+            //loop through each account data and write them to the file
             for(String element : accountArray){
-                writer.write(element+newLine);
+                writer.write(element+newLine);//write data and new line at the end
             }
-            writer.flush();
-            writer.close();
+            writer.flush();// Ensure all data is written from the buffer
+            writer.close();//close file writer
         } catch (IOException e){
             e.printStackTrace();
         }
     }
     
+    // Updates the accountArray using its account index with the latest data from the object
     public void flushAccount(){
         String data = name+","+address+","+accountNum+","+accountType+","+balance+"";
         accountArray.set(accountIndex, data);
     }
     
+    // Subtracts the withdrawal amount from the account balance and returns the new balance
     public Double withdrawal(Double amount){
         balance = balance-amount;
         return balance;
     }
     
+    // adds the deposit amount to the account balance and returns the new balance
     public Double deposit(Double amount){
         balance = balance+amount;
         return balance;
     }
-    
+    //method to get account holder's name
     public String name(){
         return name;
     }
-    
+    //method to get account holder's address
     public String address(){
         return address;
     }
-    
+    //method to get account holder's account number
     public String accountNum(){
         return accountNum;
     }
-    
+    //method to get account holder's account type
     public String accountType(){
         return accountType;
     }
-    
+    //method to get account holder's balance
     public Double balance(){
         return balance;
     }
--- bankMain.java
+++ bankMain.java
@@ -11,54 +11,64 @@ public class bankMain{
     //and then return that input.
     public static int getIntInRange(int x, int y){
         Scanner keyboard =  new Scanner(System.in);
+        //infinite loop that will break if there is return
         while(true){
+            // Check if the next input is an integer
             if(keyboard.hasNextInt()){
                 int num = keyboard.nextInt();
+                // Check if the number is within the valid range 
                 if(!(x<=num && y>=num)){
-                    System.out.println("Please type an integer in range "+x+"-"+y+".");
+                    System.out.println("Please type an integer in range "+x+"-"+y+".");//if not prompt the user
                 } 
                 else{
-                    return num;
+                    return num;//return valid input
                 }
             } 
             else{
-                keyboard.nextLine();
-                System.out.println("Please type an integer");    
+                keyboard.nextLine();//clear the invalid input
+                System.out.println("Please type an integer");//prompt the user to type number
             }
         }
     }
-
+    //This method will get a positive double
+    // It keeps asking until a valid input are entered
     public static double getDouble(){
         Scanner keyboard = new Scanner(System.in);
+        //infinite loop that will break if there is return
         while(true){
+            // Check if the next input is a valid double
             if(keyboard.hasNextDouble()){
                 double num = keyboard.nextDouble();
+                //make sure the number is positive
                 if(num>=0){
-                    return num;
+                    return num;//reutrn the number
                 }
                 else{
-                    System.out.println("Please type in a positive amount");
+                    System.out.println("Please type in a positive amount");//tell the user to type in positive number
                 }
             }
             else{
                 keyboard.nextLine();// Clear the invalid input
-                System.out.println("Please type in a valid amount.");
+                System.out.println("Please type in a valid amount.");//prompt the user to type number
             }
         }
     }
-
+    //This method will ask the user for their name, address, and the account type 
+    //Then create a new account for the user
     public static void makeNewAccount(){
         Scanner keyboard = new Scanner(System.in);
-        boolean notProceed = true;
+        boolean notProceed = true;// Used to repeat the process if user chooses to redo
         while(notProceed){
-            System.out.println("Please enter your name");
+            System.out.println("Please enter your name");//aks the user to input their name
             String userName = keyboard.nextLine();
-            System.out.println("Please enter your address. Make sure not to put any comma.");
+            System.out.println("Please enter your address. Make sure not to put any comma.");//ask user the input their address without any comma
             String address = keyboard.nextLine();
-            address = address.replace(",","");//incase suser acidently type in comma
+            address = address.replace(",","");//incase user acidently type in comma
+            // Ask the user to select an account type by number
             System.out.println("Please choose account type, the option are\n1)Everyday\n2)Saving\n3)Current\nchoose by typing the number accordingly");
-            int choice = getIntInRange(1, 3);
+            int choice = getIntInRange(1, 3);// Ensure input is within valid range
             String type;
+            //convert numeric choice to string
             switch (choice){
                 case 1 : type = "Everyday";
                     break;
@@ -68,13 +78,19 @@ public class bankMain{
                     break;
                 default: type = "Unknown";
             }
+            //Display the entered information for confirmation
             System.out.println("Name: "+userName+"\nAddress: "+address+"\nAccount type: "+type);
+            //aks if the user want to proceed
             System.out.println("Please check the information above and choose the options\n1) To proceed\n2) To redo.");
             int i = getIntInRange(1, 2);
+            //if they want to proceed
             if(i == 1){
-                notProceed = false;
+                notProceed = false;//to stop the loop
+                //call method to genarate account and store the account number that are returned
                 String num = Account.createAccount(userName, address, choice);
+                //show the account number to the user
                 System.out.println("Your account number is "+num+" please write this down for next time.");
+                // Write the new account details to the file
                 Account.writeToFile();
             }
         }
@@ -82,10 +98,12 @@ public class bankMain{
 
     public static void main(String args[]){
         Scanner keyboard = new Scanner(System.in);
+        //Load all the account data from the csv file into arrayList
         Account.loadAccount();
         
-        boolean end = false;
+        boolean end = false;//The user can keep using the program until they want to exit
         while(!end){
+            //ascii art for "kawaii-banck"
             System.out.println("  _  __                   _ _        ____              _    ");
             System.out.println(" | |/ /                  (_|_)      |  _ \\            | |   ");
             System.out.println(" | ' / __ ___      ____ _ _ _ ______| |_) | __ _ _ __ | | __");
@@ -93,90 +111,119 @@ public class bankMain{
             System.out.println(" | . \\ (_| |\\ V  V / (_| | | |      | |_) | (_| | | | |   < ");
             System.out.println(" |_|\\_\\__,_| \\_/\\_/ \\__,_|_|_|      |____/ \\__,_|_| |_|_|\\_\\");
             
+            //ask what the user want to do 
             System.out.println("what would you like to do. Please choose the options below.\n1) Acces an existing account or\n2) Create new account");
-            int menuChoice = getIntInRange(1, 2);
+            int menuChoice = getIntInRange(1, 2);// Ensure input is within valid range
+            //if the user want to create account
             if(menuChoice == 2){
-                makeNewAccount();
+                makeNewAccount();//make new account
                 System.out.println("Please choose the following options:\n1)Back to home page\n2)Exit program");
-                int option = getIntInRange(1,2);
+                int option = getIntInRange(1,2);// Ensure input is within valid range
                 if(option == 2){
-                    end = true;
+                    end = true;//Exit program
                 }
                 System.out.print('\u000C');//clear screen
             } 
+            //if the user want to continue with existing account
             else{
+                //Get the iser name
                 System.out.println("Please type in your name.");
                 String name = keyboard.nextLine();
+                //Get the account number
                 System.out.println("Please Type in the account number:\nxx-xxxx-xxxxxxx-xx");
                 String number = keyboard.nextLine();
+                //load user account information by create an account object
                 Account userAccount = new Account(name.toLowerCase(), number);
+                
+                //If the account are not found
                 if(userAccount.name() == null){
-                    System.out.println("There is no account under this name. Please choose the following opption:\n1)Back to home page\n2)Create a new account\n3)Exit program");
-                    int x  = getIntInRange(1, 3);
+                    // Offer options to go back, create a new account, or exit
+                    System.out.println("There is no account under this name. Please make sure the name and numbers are correct");
+                    System.out.println("Please choose the following opption:\n1)Back to home page\n2)Create a new account\n3)Exit program");
+                    int x  = getIntInRange(1, 3);// Ensure input is within valid range
+                    //Make new account
                     if( x == 2){
                         makeNewAccount();
                         System.out.println("Please choose the following options:\n1)Back to home page\n2)Exit program");
                         int option = getIntInRange(1,2);
                         if(option == 2){
-                            end = true;
+                            end = true;//Exit the program
                         }
-                        System.out.print('\u000C');//clear screen
-                    } else if(x == 3){
+                    }
+                    //Exit program
+                    else if(x == 3){
                         end = true;
                     }
+                    System.out.print('\u000C');//clear screen
                 }
                 else {  
-                    boolean finish = false;
-                    System.out.println("Hi "+name+"! Welcome to your account.\nname: "+userAccount.name()+"\naddres: "+userAccount.address());
-                    System.out.println("Account number: "+userAccount.accountNum()+"\nAccount type: "+userAccount.accountType()+"\nBalance: "+userAccount.balance());
+                    boolean finish = false;//The user can keep doing transaction util they satisfy
+                    
+                    //Display welcome message and account details
+                    System.out.println("Hi " + userAccount.name() + "! Welcome to your account.");
+                    System.out.println("Name: " + userAccount.name() + "\nAddress: " + userAccount.address());
+                    System.out.println("Account number: " + userAccount.accountNum());
+                    System.out.println("Account type: " + userAccount.accountType());
+                    System.out.println("Balance: " + userAccount.balance());
+                    
+                    //loop trought transaction mene
                     while(!finish){    
                         System.out.println("What would you like to do.\n1)Deposit\n2)Withdraw");
-                        int x = getIntInRange(1, 3);
+                        int x = getIntInRange(1, 2);// Ensure input is within valid range
+                        //deposite
                         if (x==1){
                             System.out.println("Please type the amount that you wish to deposit.");
-                            double amount = getDouble();
+                            double amount = getDouble();//method to get a valid double input
                             System.out.println("Your current balance is " + userAccount.deposit(amount));
-                            userAccount.flushAccount();
+                            userAccount.flushAccount();//Save updated balance to arrauList
                         } 
+                        //withdrawal
                         else{
-                            System.out.println("Please type the amount that you wish to withdraw.");
-                            double amount = getDouble();
+                            System.out.println("Please type the amount that you wish to withdrawal.");
+                            double amount = getDouble();//method to get a valid double input
+                            //If the account type is cuurent
                             if(userAccount.accountType().equals( "Current")){
+                                //Current accounts allow overdraft up to -1000
                                 if(userAccount.balance()-amount<-1000){
                                     System.out.println("Your account does not have sufficient fund, the overdraft limit is 1000$");
                                     System.out.println("You now have " +userAccount.balance()+ "$ in your account");
                                 }
+                                //other wise withdrawalthe money
                                 else{
-                                    userAccount.withdrawal(amount);
+                                    userAccount.withdrawal(amount);//withdrawal method to withdrawal
                                     System.out.println("You now have " +userAccount.balance()+ "$ remaining in your account");
                                 }
                             }
+                            // Other accounts cannot be overdrawn
                             else {
+                                //If balance after withdrawal is less than 0
                                 if(userAccount.balance()-amount<0){
                                     System.out.println("Your account does not have sufficient fund, the account does not alowed overdraft");
                                 }
+                                //other wise withdrawalthe money
                                 else{
-                                    userAccount.withdrawal(amount);
+                                    userAccount.withdrawal(amount);//withdrawal method to withdrawal
                                     System.out.println("You now have " +userAccount.balance()+ "$ remaining in your account");
                                 }
                             }
-                            userAccount.flushAccount();
+                            userAccount.flushAccount();//save and update this account to the array list
                         }
-    
+                        //Ask if the user wants to do another transaction, go home, or exit
                         System.out.println("Plese choose the following options:\n1)Do more transaction\n2)Back to home page\n3)Exit program");
                         int y = getIntInRange(1, 3);
                         if (y==2){
-                            finish = true;
+                            finish = true;//Exit transaction page and go to home page
                             System.out.print('\u000C');//clear screen
                         } else if (y==3){
-                            finish = true;
-                            end = true;
+                            finish = true;//Exit transaction page
+                            end = true;//Exit program
                         }
                     }
                 }
             }
         }
-        Account.writeToFile();
+        System.out.print('\u000C');//clear screen
+        Account.writeToFile();//write all the updated account information to the csv file
         System.out.println("Thank you for using  Kawaii bank and we hope to see you again soon!");
     }
 }
